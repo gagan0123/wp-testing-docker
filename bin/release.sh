dfdfcbd7e1f018ac185141e5f935d90e2ce3ceeb
Gagan Deep Singh <me@gagan.pro>
2019-02-26 22:04:10 +0530

    Using travis folds to make the release output viewable

--- bin/release.sh
+++ bin/release.sh
@@ -18,6 +18,15 @@ CONTAINERS=(
 # Login to docker
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# Travis fold functions
+fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
+
 # Looping through tags and releasing each tag
 for TAG in ${CONTAINERS[*]}
 do
@@ -25,8 +34,10 @@ do
     export CONTAINER_TEST_IMAGE="$DEV_REPO:$TAG-$TRAVIS_COMMIT";
     export CONTAINER_RELEASE_IMAGE="$RELEASE_REPO:$TAG";
 
+    fold_start "$TAG" "Releasing tag $TAG from $CONTAINER_TEST_IMAGE to $CONTAINER_RELEASE_IMAGE"
     # Releasing current image
     docker pull $CONTAINER_TEST_IMAGE;
     docker tag $CONTAINER_TEST_IMAGE $CONTAINER_RELEASE_IMAGE;
     docker push $CONTAINER_RELEASE_IMAGE
+    fold_end "$TAG"
 done
