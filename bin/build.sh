26d707dc51ac8f1e70b6c187109b5c7ece5009d9
Gagan Deep Singh <me@gagan.pro>
2019-02-26 22:41:57 +0530

    Streamlining travis output on builds/tests

--- bin/build.sh
+++ bin/build.sh
@@ -4,15 +4,32 @@
 export CONTAINER_TEST_IMAGE="$DEV_REPO:$TAG-$TRAVIS_COMMIT"
 export CONTAINER_RELEASE_IMAGE="$RELEASE_REPO:$TAG"
 
+# Travis fold functions
+fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
+
 # Change directory to our Dockerfile
 cd $FOLDER
 
 # Login to docker
+fold_start "dockerlogin" "Logging in to Docker"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fold_end "dockerlogin"
 
 # Build Docker image using cache
+fold_start "dockerpull" "Pulling from $$CONTAINER_RELEASE_IMAGE"
 docker pull $CONTAINER_RELEASE_IMAGE
+fold_end "dockerpull"
+fold_start "dockerbuild" "Building Image $CONTAINER_TEST_IMAGE"
 docker build --cache-from $CONTAINER_RELEASE_IMAGE -t $CONTAINER_TEST_IMAGE .
+fold_end "dockerbuild"
 
 # Push built image to docker
+fold_start "dockerpush" "Pusing Image $CONTAINER_TEST_IMAGE"
 docker push $CONTAINER_TEST_IMAGE
+fold_end "dockerpush"
