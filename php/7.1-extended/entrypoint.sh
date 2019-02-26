ac7528e25d54f4cf3004d0681c92682711ca68bb
Gagan Deep Singh <me@gagan.pro>
2019-02-26 23:44:01 +0530

    Ability to use different PHPUnit version during runtime

--- php/7.1-extended/entrypoint.sh
+++ php/7.1-extended/entrypoint.sh
@@ -4,9 +4,14 @@ set -e
 # Provide github token if you are using composer a lot in non-interactive mode
 # Otherwise one day it will get stuck with request for authorization
 # https://github.com/settings/tokens
-if [[ ! -z "$COMPOSER_GITHUB" ]]
-then
-    composer config --global github-oauth.github.com "$COMPOSER_GITHUB"
+if [[ ! -z "$COMPOSER_GITHUB" ]]; then
+    composer config --global github-oauth.github.com "$COMPOSER_GITHUB";
+fi
+
+# If specified we can switch the phpunit version
+if [[ ! -z "$PHPUNIT_VERSION" ]]; then
+    curl -L https://phar.phpunit.de/phpunit-$PHPUNIT_VERSION.phar -o /usr/local/bin/phpunit && \
+    chmod +x /usr/local/bin/phpunit
 fi
 
 if [ $# -eq 0 ]; then
